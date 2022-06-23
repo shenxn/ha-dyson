d859632854f5596da48df7ff6d0306366888262f
Dobby <dobby5nk@gmail.com>
2022-06-23 22:46:14 +0200

    Fixing the error when adjusting the temperature in the thermostat (Automatic switching to heat mode) Adding switch "Auto Mode" and "Oscillation"

--- custom_components/dyson_local/climate.py
+++ custom_components/dyson_local/climate.py
@@ -20,6 +20,7 @@ from homeassistant.components.climate.const import (
     SUPPORT_FAN_MODE,
     SUPPORT_TARGET_TEMPERATURE,
 )
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_TEMPERATURE, CONF_NAME, TEMP_CELSIUS
 from homeassistant.core import Callable, HomeAssistant
@@ -33,7 +34,6 @@ HVAC_MODES = [HVAC_MODE_OFF, HVAC_MODE_COOL, HVAC_MODE_HEAT]
 FAN_MODES = [FAN_FOCUS, FAN_DIFFUSE]
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE
 SUPPORT_FLAGS_LINK = SUPPORT_FLAGS | SUPPORT_FAN_MODE
-
 
 async def async_setup_entry(
     hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities: Callable
@@ -112,11 +112,15 @@ class DysonClimateEntity(DysonEntity, ClimateEntity):
     @property
     def min_temp(self):
         """Return the minimum temperature."""
+        if not self._device.heat_mode_is_on:
+            return 1
         return 1
 
     @property
     def max_temp(self):
         """Return the maximum temperature."""
+        if not self._device.heat_mode_is_on:
+            return 10
         return 37
 
     def set_temperature(self, **kwargs):
@@ -129,7 +133,10 @@ class DysonClimateEntity(DysonEntity, ClimateEntity):
         # Limit the target temperature into acceptable range.
         target_temp = min(self.max_temp, target_temp)
         target_temp = max(self.min_temp, target_temp)
-        self._device.set_heat_target(target_temp + 273)
+        if self._device.heat_mode_is_on:
+            self._device.set_heat_target(target_temp + 273)
+        else:
+            self._device.set_speed(int(target_temp))
 
     def set_hvac_mode(self, hvac_mode: str):
         """Set new hvac mode."""
